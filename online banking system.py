2d14fc4d079ad25a9a93557d0e04d92c0c850578
shreya-ghub <114590507+shreya-ghub@users.noreply.github.com>
2022-11-05 17:28:43 +0530

    Update online banking system.py

--- online banking system.py
+++ online banking system.py
@@ -126,3 +126,269 @@ def signup(user_accounts, log_in, username, password):
             return True
         else:
             if valid(password)==True:
+                user_accounts[username]=password
+                log_in[username]=False
+            else:
+                return False
+import re
+
+def valid(password):
+
+    if(len(password)>=8):
+        regex = ("^(?=.*[a-z])(?=." +
+                "[A-Z])(?=.\\d)")
+        p = re.compile(regex)
+        if (re.search(p, password)):
+            return True
+        else:
+            return False
+			
+			
+			
+			
+			
+			
+def import_and_create_accounts(filename):
+    '''
+    This function is used to create an user accounts dictionary and another login dictionary.  The given argument is the
+    filename to load.
+    Every line in the file should be in the following format:
+      username - password
+    The key is a username and the value is a password.  If the username and password fulfills the requirements,
+    add the username and password into the user accounts dictionary.  To make sure that the password fulfills these
+    requirements, be sure to use the signup function that you wrote above.
+
+    For the login dictionary, the key is the username, and its value indicates whether the user is logged in, or not.
+    Initially, all users are not logged in.
+
+    What you will do:
+    - Create an empty user accounts dictionary and an empty login dictionary.
+    - Read in the file.
+    - If the username and password fulfills the requirements, adds the username and password
+    into the user accounts dictionary, and updates the login dictionary.
+    - You should also handle the following cases:
+    -- When the password is missing.  If so, ignore that line and don't update the dictionaries.
+    -- When there is whitespace at the beginning or end of a line and/or between the name and password on a line.  You
+    should trim any and all whitespace.
+    - Return both the user accounts dictionary and login dictionary from this function.
+
+    For example, here's how your code should handle some specific lines in the file:
+    The 1st line in the file has a name and password:
+      Brandon - brandon123ABC
+    Your code will process this line, and using the signup function, will add the extracted information to the
+    dictionaries.  After it does, the dictionaries will look like this:
+      user_accounts = {"Brandon": "brandon123ABC"}
+      log_in = {"Brandon": False}
+
+    The 2nd line in the file has a name but missing password:
+      Jack
+    Your code will ignore this line.  The dictionaries will still look like this:
+      user_accounts = {"Brandon": "brandon123ABC"}
+      log_in = {"Brandon": False}
+
+    The 3rd line in the file has a name and password:
+      Jack - jac123
+    Your code will process this line, and using the signup function, will not add the extracted information to the
+    dictionaries because the password is invalid.  The dictionaries will still look like this:
+      user_accounts = {"Brandon": "brandon123ABC"}
+      log_in = {"Brandon": False}
+
+    The 4th line in the file has a name and password:
+      Jack - jack123POU
+    Your code will process this line, and using the signup function, will add the extracted information to the
+    dictionaries.  After it does, the dictionaries will look like this:
+      user_accounts = {"Brandon": "brandon123ABC, "Jack": "jack123POU"}
+      log_in = {"Brandon": False, "Jack": False}
+
+    After processing every line in the file, the dictionaries will look like this:
+      user_accounts = {"Brandon": "brandon123ABC, "Jack": "jack123POU", "James": "100jamesABD", "Sarah": "sd896ssfJJH"}
+      log_in = {"Brandon": False, "Jack": False, "James": False, "Sarah": False}
+    Return the dictionaries from this function.
+    '''
+    user_accounts = {}
+    log_in = {}
+    with open(filename,"r") as data:
+        lines=data.readlines()
+        for i in lines:
+            new=i.strip().split("-")
+            if len(new)<=1:
+                continue
+            key=new[0].strip()
+            value=new[1].strip()
+            try:
+                signup(user_accounts,log_in,key,value);
+            except:
+                continue
+        return user_accounts,log_in
+		
+		
+		
+		
+		
+		
+def login(user_accounts, log_in, username, password):
+    '''
+    This function allows users to log in with their username and password.
+    The user_accounts dictionary stores the username and associated password.
+    The log_in dictionary stores the username and associated log-in status.
+
+    If the username does not exist in user_accounts or the password is incorrect:
+    - Returns False.
+    Otherwise:
+    - Updates the user's log-in status in the log_in dictionary, setting the value to True.
+    - Returns True.
+
+    For example:
+    - Calling login(user_accounts, "Brandon", "123abcAB") will return False
+    - Calling login(user_accounts, "Brandon", "brandon123ABC") will return True
+    '''
+
+    if username in user_accounts:
+        if user_accounts[username]==password:
+            log_in[username]=True
+            return True
+        else:
+            return False
+    else:
+        return False
+		
+		
+		
+		
+		
+
+def update(bank, log_in, username, amount):
+    '''
+    In this function, you will try to update the given user's bank account with the given amount.
+    bank is a dictionary where the key is the username and the value is the user's account balance.
+    log_in is a dictionary where the key is the username and the value is the user's log-in status.
+    amount is the amount to update with, and can either be positive or negative.
+
+    To update the user's account with the amount, the following requirements must be met:
+    - The user exists in log_in and his/her status is True, meaning, the user is logged in.
+
+    If the user doesn't exist in the bank, create the user.
+    - The given amount can not result in a negative balance in the bank account.
+
+    Return True if the user's account was updated.
+
+    For example, if Brandon has 115.50 in his account:
+    - Calling update(bank, log_in, "Brandon", 50) will return False, unless "Brandon" is first logged in.  Then it
+    will return True.  Brandon will then have 165.50 in his account.
+    - Calling update(bank, log_in, "Brandon", -200) will return False because Brandon does not have enough in his
+    account.
+    '''
+
+    # your code here
+    if username in bank:
+        if log_in[username] == True:
+            if amount + bank[username] >= 0:
+                bank[username] = bank[username] + amount
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        bank[username] = amount 
+        return True
+		
+		
+		
+		
+		
+		
+		
+def transfer(bank, log_in, userA, userB, amount):
+    '''
+    In this function, you will try to make a transfer between two user accounts.
+    bank is a dictionary where the key is the username and the value is the user's account balance.
+    log_in is a dictionary where the key is the username and the value is the user's log-in status.
+    amount is the amount to be transferred between user accounts (userA and userB).  amount is always positive.
+
+    What you will do:
+    - Deduct the given amount from userA and add it to userB, which makes a transfer.
+    - You should consider some following cases:
+      - userA must be in the bank and his/her log-in status in log_in must be True.
+      - userB must be in log_in, regardless of log-in status.  userB can be absent in the bank.
+      - No user can have a negative amount in their account. He/she must have a positive or zero balance.
+
+    Return True if a transfer is made.
+
+    For example:
+    - Calling transfer(bank, log_in, "BrandonK", "Jack", 100) will return False
+    - Calling transfer(bank, log_in, "Brandon", "JackC", 100) will return False
+    - After logging "Brandon" in, calling transfer(bank, log_in, "Brandon", "Jack", 10) will return True
+    - Calling transfer(bank, log_in, "Brandon", "Jack", 200) will return False  
+    '''
+
+    # your code here
+    if userA in bank:
+        if log_in[userA] == True:
+            if userB in log_in:
+                if userB in bank:
+                    if bank[userA] - amount >= 0:
+                        bank[userA] = bank[userA] - amount
+                        bank[userB] = bank[userB] + amount
+                        return True
+                else:
+                    bank[userB] = 0
+                    if bank[userA] - amount >= 0:
+                        bank[userA] = bank[userA] - amount
+                        bank[userB] = bank[userB] + amount
+                        return True
+                    else:
+                        return False
+                        
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+		
+		
+		
+		
+		
+		
+		
+def change_password(user_accounts, log_in, username, old_password, new_password):
+    '''
+    This function allows users to change their password.
+
+    If all of the following requirements are met, changes the password and returns True. Otherwise, returns False.
+    - The username exists in the user_accounts.
+    - The user is logged in (the username is associated with the value True in the log_in dictionary)
+    - The old_password is the user's current password.
+    - The new_password should be different from the old one.
+    - The new_password fulfills the requirement in signup.
+
+    For example:
+    - Calling change_password(user_accounts, log_in, "BrandonK", "123abcABC" ,"123abcABCD") will return False
+    - Calling change_password(user_accounts, log_in, "Brandon", "123abcABCD", "123abcABCDE") will return False
+    - Calling change_password(user_accounts, log_in, "Brandon", "brandon123ABC", "brandon123ABC") will return False
+    - Calling change_password(user_accounts, log_in, "Brandon", "brandon123ABC", c"123abcABCD") will return True
+
+    Hint: Think about defining and using a separate valid(password) function that checks the validity of a given password.
+    This will also come in handy when writing the signup() function.
+    '''
+
+    # your code here
+    if username in user_accounts:
+        if log_in[username]==True:
+            if user_accounts[username]==old_password:
+                if old_password!=new_password:
+                    if(valid(new_password)==True):
+                        user_accounts[username]=new_password
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
